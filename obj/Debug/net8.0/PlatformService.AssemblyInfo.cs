2a38cb01a89b4b950c231f103f0b981d739afb43
Tagor Petrosian <tagorpe@matrix.com>
2025-01-20 19:58:27 +0200

    packages and readme

--- obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
+++ obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81afd6e88ce97a3a67691fb0403ac6330b592fb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8980d2f68ab3445f8ddbe9612b6ff49982b364db")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
